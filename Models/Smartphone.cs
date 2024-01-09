bbbd04048c2ce2886da2b41a90debfece5b96591
Samuel Lira <samuel.slira@hotmail.com>
2024-01-08 23:58:11 -0300

    DESAFIO CONCLUÍDO git status

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -4,11 +4,22 @@ namespace DesafioPOO.Models
     {
         public string Numero { get; set; }
         // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        public string Modelo {get;set;}
+        public string IMEI {get;set;}
+        public int Memoria {get;set;}
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
             // TODO: Passar os parâmetros do construtor para as propriedades
+        }
+
+        protected Smartphone(string numero)
+        {
+            Numero = numero;
         }
 
         public void Ligar()
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone Nokia = new Nokia("1234", "Modelo 1", "132245", 32);
+Nokia.Ligar();
+Nokia.ReceberLigacao();
+Nokia.InstalarAplicativo("Telegram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+Smartphone iphone = new Iphone("4321", "Modelo 2", "6543321", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
